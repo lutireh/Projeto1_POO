bc9c8fdd44ddb3097705d32e79b9edf73399967e
ferfsnandes <112121591+ferfsnandes@users.noreply.github.com>
2022-09-12 21:31:38 -0300

    Update WordGraph.java

--- src/WordGraph.java
+++ src/WordGraph.java
@@ -5,34 +5,34 @@ import java.io.Writer;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.TreeSet;
-
-public class WordGraph {
     /** This class sorts the words into a list and exports them in a CVS archive;
      *
      *@autor group4
-     *@version 1.?
-     *
      */
-    private TreeMap<String, TreeSet<String>> graph = new TreeMap<>();
-    /** Method for ?
+public class WordGraph {
+  
+      /** Method for conect two words 
      *
      * @param word1
      * @param word2
      */
-    public void connect(String word1, String word2) {
-/** method that (arrange/sort) the words into adjacency lists;
+    private TreeMap<String, TreeSet<String>> graph = new TreeMap<>();
+    
+  /** method that (arrange/sort) the words into adjacency lists;
  *
  */
+    public void connect(String word1, String word2) {
+
         if (graph.get(word1) == null) {
             graph.put(word1, new TreeSet<>());
         }
         graph.get(word1).add(word2);
     }
-
-    public void exportCSV(String outFile) throws IOException {
-        /** Method responsible for receive the list, convert and export the list in a CVS archive form;
+   /** Method responsible for receive the list, convert and export the list in a CVS archive form;
          *
          */
+    public void exportCSV(String outFile) throws IOException {
+     
         try (Writer out = new BufferedWriter(new FileWriter(outFile))) {
             for (Entry<String, TreeSet<String>> entry : graph.entrySet()) {
                 out.write(entry.getKey());
